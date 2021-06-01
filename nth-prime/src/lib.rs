834027bda325f639e23c0da3c1a841f6eb5a34db
Soushi888 <Sacha.pignot@protonmail.com>
2021-06-01 02:46:16 -0400

    nth-prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,27 @@
+fn is_prime(n: u32) -> bool {
+    if n <= 1 { return false; }
+
+    for i in 1..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes_vec: Vec<u32> = vec![2, 3];
+    let mut i: u32 = 0;
+    loop {
+        if primes_vec.len() >= n as usize { break; }
+
+        if is_prime(i) {
+            primes_vec.push(i)
+        }
+
+        i += 1;
+    }
+
+
+    primes_vec[n as usize]
 }
