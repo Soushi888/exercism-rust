b21f374b4cba3dddb807b875389679d26af41cf5
Soushi888 <Sacha.pignot@protonmail.com>
2021-06-03 22:30:39 -0400

    proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut proverb: Vec<String> = vec![];
+
+    if list.len() > 1 {
+        for (i, word) in list.iter().enumerate() {
+            if i < list.len() - 1 {
+                proverb.push(format!("For want of a {} the {} was lost.", word, list[i + 1]));
+            }
+        }
+    }
+
+    if list.len() > 0 { proverb.push(format!("And all for the want of a {}.", list[0])); }
+
+    proverb.join("\n")
 }
