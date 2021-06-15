126efb6a96206f8bda071cf0f9e27f0502789a23
Soushi888 <Sacha.pignot@protonmail.com>
2021-06-15 01:29:27 -0400

    clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,5 +1,8 @@
 use std::fmt::{Display, Formatter, Result};
 
+const HOURS_PER_DAY: i32 = 24;
+const MINUTES_PER_HOUR: i32 = 60;
+
 #[derive(Debug, PartialEq)]
 pub struct Clock {
     hours: i32,
@@ -8,28 +11,28 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(mut hours: i32, mut minutes: i32) -> Self {
-        let minutes_cycles_number = minutes / 60;
-        minutes = minutes - (60 * minutes_cycles_number);
+        let minutes_cycles_number = minutes / MINUTES_PER_HOUR;
+        minutes = minutes - (MINUTES_PER_HOUR * minutes_cycles_number);
 
         hours += minutes_cycles_number;
-        let hours_cycles_number = hours / 24;
-        hours = hours - (24 * hours_cycles_number);
+        let hours_cycles_number = hours / HOURS_PER_DAY;
+        hours = hours - (HOURS_PER_DAY * hours_cycles_number);
 
-        if hours >= 24 {
+        if hours >= HOURS_PER_DAY {
             hours = 0 + (minutes_cycles_number - (if hours_cycles_number > 0 {
-                24 + hours_cycles_number
+                HOURS_PER_DAY + hours_cycles_number
             } else {
-                24
+                HOURS_PER_DAY
             }));
         };
 
         if minutes < 0 {
-            minutes += 60;
+            minutes += MINUTES_PER_HOUR;
             hours -= 1;
         }
 
         if hours < 0 {
-            hours += 24;
+            hours += HOURS_PER_DAY;
         }
 
         Clock {
@@ -47,21 +50,6 @@ impl Clock {
 
 impl Display for Clock {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        let hours: String;
-        let minutes: String;
-
-        if self.hours < 10 {
-            hours = format!("0{}", self.hours)
-        } else {
-            hours = self.hours.to_string()
-        }
-
-        if self.minutes < 10 {
-            minutes = format!("0{}", self.minutes)
-        } else {
-            minutes = self.minutes.to_string()
-        }
-
-        write!(f, "{}:{}", hours, minutes)
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
