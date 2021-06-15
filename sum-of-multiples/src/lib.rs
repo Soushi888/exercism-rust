9f5f892851f61402abc52cfbbfecbb8390ffac57
Soushi888 <Sacha.pignot@protonmail.com>
2021-06-15 01:29:44 -0400

    sum-of-multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -5,12 +5,17 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
 
     let mut current_multiple: u32 = factors[0];
     let mut sum: u32 = 0;
+    let mut is_doubled: bool;
 
     while current_multiple < limit {
+        is_doubled = false;
+
         for i in 0..factors.len() {
-            if current_multiple % factors[i] == 0 {
+            if current_multiple % factors[i] == 0 && !is_doubled {
                 sum += current_multiple;
                 println!("i = {}, sum = {}", i, sum);
+
+                is_doubled = true;
             }
         }
         println!("current multiple = {}, sum = {}", current_multiple, sum);
