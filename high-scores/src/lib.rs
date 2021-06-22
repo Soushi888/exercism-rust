aa9f56bab99c316bf86fa23081a2eae81d92c96b
Soushi888 <Sacha.pignot@protonmail.com>
2021-06-22 00:33:36 -0400

    high-scores

--- high-scores/src/lib.rs
+++ high-scores/src/lib.rs
@@ -1,27 +1,25 @@
 #[derive(Debug)]
-pub struct HighScores {
-    scores: Vec<u32>,
-}
+pub struct HighScores(Vec<u32>);
 
 impl HighScores {
     pub fn new(scores: &[u32]) -> Self {
-        HighScores { scores: scores.to_vec() }
+        HighScores(scores.to_vec())
     }
 
     pub fn scores(&self) -> &[u32] {
-        self.scores.as_slice()
+        self.0.as_slice()
     }
 
     pub fn latest(self) -> Option<u32> {
-        self.scores.last().cloned()
+        self.0.last().cloned()
     }
 
-    pub fn personal_best(&self) -> Option<u32> {
-        self.scores.iter().max().cloned()
+    pub fn personal_best(self) -> Option<u32> {
+        self.0.iter().max().cloned()
     }
 
     pub fn personal_top_three(self) -> Vec<u32> {
-        let mut sorted_scores = self.scores;
+        let mut sorted_scores = self.0;
         sorted_scores.sort();
 
         sorted_scores.iter().rev().take(3).cloned().collect()
