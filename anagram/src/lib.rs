a5bbe9a24009786199e6dc1c18d507544e0d3139
Soushi888 <Sacha.pignot@protonmail.com>
2021-06-22 23:32:43 -0400

    anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,30 +2,20 @@ use std::collections::HashSet;
 
 /// Return true if word_1 and word_2 are anagrams.
 pub fn is_anagram(word_1: &str, word_2: &str) -> bool {
-    let mut word_1_letters: Vec<char> = word_1.to_lowercase().chars().collect();
-    word_1_letters.sort();
+    let word_1_lower = word_1.to_lowercase();
+    let word_2_lower = word_2.to_lowercase();
 
-    let mut word_2_letters: Vec<char> = word_2.to_lowercase().chars().collect();
-    word_2_letters.sort();
+    let mut word_1_letters: Vec<char> = word_1_lower.chars().collect();
+    word_1_letters.sort_unstable();
 
-    if word_1.to_lowercase() != word_2.to_lowercase() {
-        word_1_letters == word_2_letters
-    } else {
-        false
-    }
+    let mut word_2_letters: Vec<char> = word_2_lower.chars().collect();
+    word_2_letters.sort_unstable();
+
+    (word_1_lower != word_2_lower) && (word_1_letters == word_2_letters)
 }
 
 /// Return an HashSet of anagrams among a dictionary array for a specific word.
 pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut output = HashSet::new();
-
-    for i in 0..possible_anagrams.len() {
-        let possible_anagram = possible_anagrams[i];
-
-        if is_anagram(word, possible_anagram) {
-            output.insert(possible_anagram);
-        }
-    }
-
-    output
+    possible_anagrams.iter().filter(|candidate| is_anagram(word, candidate))
+        .copied().collect()
 }
