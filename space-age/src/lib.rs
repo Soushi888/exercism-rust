6018875fa38f0a291e8b3f4f8a5e58e9eb0c8681
Soushi888 <Sacha.pignot@protonmail.com>
2021-06-28 16:52:55 -0400

    space-age

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -1,5 +1,4 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
+const SECONDS_PER_YEARS: f64 = 31557600.0;
 
 #[derive(Debug)]
 pub struct Duration(u64);
@@ -33,55 +32,55 @@ pub struct Neptune;
 impl Planet for Mercury {
     fn years_during(d: &Duration) -> f64 {
         let speed: f64 = 0.2408467;
-        f64::trunc((d.0 as f64 / 60.0 / 60.0 / 24.0 / 365.25 / speed) * 100.0) / 100.0
+        d.0 as f64 / SECONDS_PER_YEARS / speed
     }
 }
 
 impl Planet for Venus {
     fn years_during(d: &Duration) -> f64 {
         let speed: f64 = 0.61519726;
-        f64::trunc((d.0 as f64 / 60.0 / 60.0 / 24.0 / 365.25 / speed) * 100.0) / 100.0
+        d.0 as f64 / SECONDS_PER_YEARS / speed
     }
 }
 
 impl Planet for Earth {
     fn years_during(d: &Duration) -> f64 {
         let speed: f64 = 1.0;
-        f64::trunc((d.0 as f64 / 60.0 / 60.0 / 24.0 / 365.25 / speed) * 100.0) / 100.0
+        d.0 as f64 / SECONDS_PER_YEARS / speed
     }
 }
 
 impl Planet for Mars {
     fn years_during(d: &Duration) -> f64 {
         let speed: f64 = 1.8808158;
-        f64::trunc((d.0 as f64 / 60.0 / 60.0 / 24.0 / 365.25 / speed) * 100.0) / 100.0
+        d.0 as f64 / SECONDS_PER_YEARS / speed
     }
 }
 
 impl Planet for Jupiter {
     fn years_during(d: &Duration) -> f64 {
         let speed: f64 = 11.862615;
-        f64::trunc((d.0 as f64 / 60.0 / 60.0 / 24.0 / 365.25 / speed) * 100.0) / 100.0
+        d.0 as f64 / SECONDS_PER_YEARS / speed
     }
 }
 
 impl Planet for Saturn {
     fn years_during(d: &Duration) -> f64 {
         let speed: f64 = 29.447498;
-        f64::trunc((d.0 as f64 / 60.0 / 60.0 / 24.0 / 365.25 / speed) * 100.0) / 100.0
+        d.0 as f64 / SECONDS_PER_YEARS / speed
     }
 }
 
 impl Planet for Uranus {
     fn years_during(d: &Duration) -> f64 {
         let speed: f64 = 84.016846;
-        f64::trunc((d.0 as f64 / 60.0 / 60.0 / 24.0 / 365.25 / speed) * 100.0) / 100.0
+        d.0 as f64 / SECONDS_PER_YEARS / speed
     }
 }
 
 impl Planet for Neptune {
     fn years_during(d: &Duration) -> f64 {
         let speed: f64 = 164.79132;
-        f64::trunc((d.0 as f64 / 60.0 / 60.0 / 24.0 / 365.25 / speed) * 100.0) / 100.0
+        d.0 as f64 / SECONDS_PER_YEARS / speed
     }
 }
