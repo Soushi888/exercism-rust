b28f2ad953a2676da1d799ae7c90395075a014e9
Soushi888 <Sacha.pignot@protonmail.com>
2021-07-05 17:47:03 -0400

    prime-factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,21 @@
+/// Return a vector of the factors of a given unsigned integer
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut prime_factors = vec![];
+    let mut i: u64 = 2;
+    let mut current_factor: u64 = n;
+
+    while i.pow(2) <= current_factor {
+        while current_factor % i == 0 {
+            prime_factors.push(i);
+            current_factor /= i;
+        }
+
+        i += 1;
+    }
+
+    if current_factor != 1 {
+        prime_factors.push(current_factor)
+    }
+
+    prime_factors
 }
