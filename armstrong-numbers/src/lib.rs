f9fd8e32bc9e66e3efdbf525e0e71e0441677ed4
Soushi888 <Sacha.pignot@protonmail.com>
2021-07-05 23:57:21 -0400

    armstrong-numbers

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let figures: Vec<u32> = num.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+    let mut total: u32 = 0;
+
+    for i in &figures {
+        total += i.pow(figures.len() as u32);
+    }
+
+    total == num
 }
