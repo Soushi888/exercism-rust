c0f7fee2703501dbcde11967ff5c85b30ab7ee63
soushi888 <sacha.pignot@protonmail.com>
2022-02-27 22:36:38 -0500

    sublist

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -7,5 +7,20 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list == _second_list {
+        return Comparison::Equal;
+    } else if _first_list.len() < _second_list.len() && is_sublist(_first_list, _second_list) {
+        return Comparison::Sublist;
+    } else if _second_list.len() < _first_list.len() && is_sublist(_second_list, _first_list) {
+        return Comparison::Superlist;
+    }
+
+    Comparison::Unequal
+}
+
+fn is_sublist<T: PartialEq>(pattern: &[T], search_universe: &[T]) -> bool {
+    pattern.is_empty()
+        || search_universe
+        .windows(pattern.len())
+        .any(|w| w == pattern)
 }
