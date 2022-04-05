f038c45de7aa5e7243456775bc60dc2b8e382ce9
Soushi888 <sacha.pignot@protonmail.com>
2022-04-05 05:19:37 -0400

    assembly-line

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -2,10 +2,20 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+const LOWEST_CARS_PER_HOUR: u16 = 221;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let base_production = (LOWEST_CARS_PER_HOUR * speed as u16) as f64;
+    let cars_produced: f64 = match speed {
+        0..=4 => base_production,
+        5..=8 => base_production * 0.9,
+        9..=10 => base_production * 0.77,
+        _ => panic!("Speed must be a number between 0 and 10.")
+    };
+
+    cars_produced
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    production_rate_per_hour(speed) as u32 / 60
 }
