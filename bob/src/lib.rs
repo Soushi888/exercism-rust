ff4193204eacfe1e1738ce5e015f2efd6e017ed0
soushi888 <sacha.pignot@protonmail.com>
2022-08-31 22:40:40 -0400

    bob finish

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,22 +1,26 @@
 pub fn reply(message: &str) -> &str {
     let response;
     let mut type_of_message: &str = "";
+    let message = message.trim();
 
-    if message.to_uppercase() == message { type_of_message = "yelling" }
-    if message.trim() == "" { type_of_message = "empty" }
-    if message.trim().len() != 0 {
-        if message.trim().chars().nth(message.trim().len() - 1).unwrap() == '?' { type_of_message = "question" }
-        if message.to_uppercase() == message && message.trim().chars().nth(message.trim().len() - 1).unwrap() == '?' { type_of_message = "yelled question" }
+    let is_letters = message
+        .replace("?", "")
+        .chars().any(|c| c.is_alphabetic());
+
+    if message.to_uppercase() == message && is_letters { type_of_message = "yelling" }
+    if message == "" { type_of_message = "empty" }
+    if message.len() != 0 {
+        if message.chars().nth(message.len() - 1).unwrap() == '?' { type_of_message = "question" }
+
+        if message.to_uppercase() == message && message.chars().nth(message.len() - 1).unwrap() == '?' && is_letters { type_of_message = "yelled question" }
     }
-
-    println!("{}", message.trim());
 
     match type_of_message {
         "question" => { response = "Sure." }
         "yelling" => { response = "Whoa, chill out!" }
         "yelled question" => { response = "Calm down, I know what I'm doing!" }
         "empty" => { response = "Fine. Be that way!" }
-        _ => { response = "Whatever."; }
+        _ => { response = "Whatever." }
     }
 
     response
--- bob/tests/bob.rs
+++ bob/tests/bob.rs
@@ -82,14 +82,14 @@ fn test_question_with_no_letters() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// no letters
 fn test_no_letters() {
     process_response_case("1, 2, 3", "Whatever.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// statement containing question mark
 fn test_statement_containing_question_mark() {
     process_response_case("Ending with ? means a question.", "Whatever.");
@@ -97,7 +97,7 @@ fn test_statement_containing_question_mark() {
 
 //NEW
 #[test]
-#[ignore]
+// #[ignore]
 /// multiple line question
 fn test_multiple_line_question() {
     process_response_case(
@@ -107,7 +107,7 @@ fn test_multiple_line_question() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// non-question ending with whitespace
 fn test_nonquestion_ending_with_whitespace() {
     process_response_case(
@@ -117,56 +117,56 @@ fn test_nonquestion_ending_with_whitespace() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// shouting
 fn test_shouting() {
     process_response_case("WATCH OUT!", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// non-letters with question
 fn test_nonletters_with_question() {
     process_response_case(":) ?", "Sure.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// shouting gibberish
 fn test_shouting_gibberish() {
     process_response_case("FCECDFCAAB", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// asking a question
 fn test_asking_a_question() {
     process_response_case("Does this cryogenic chamber make me look fat?", "Sure.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// asking a numeric question
 fn test_asking_a_numeric_question() {
     process_response_case("You are, what, like 15?", "Sure.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// silence
 fn test_silence() {
     process_response_case("", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// starting with whitespace
 fn test_starting_with_whitespace() {
     process_response_case("         hmmmmmmm...", "Whatever.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// using acronyms in regular speech
 fn test_using_acronyms_in_regular_speech() {
     process_response_case(
@@ -176,14 +176,14 @@ fn test_using_acronyms_in_regular_speech() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// alternate silence
 fn test_alternate_silence() {
     process_response_case("										", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// prolonged silence
 fn test_prolonged_silence() {
     process_response_case("          ", "Fine. Be that way!");
