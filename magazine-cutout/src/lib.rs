acf1ac03576796fb9283988ac8d70b6e35bb28d0
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-08 02:47:06 -0400

    magazine-cutout

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -1,9 +1,24 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut magazine_map = HashMap::new();
+    let mut note_map = HashMap::new();
+
+    for word in magazine {
+        let count = magazine_map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    for word in note {
+        let count = note_map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    for (word, count) in note_map {
+        if magazine_map.get(word).unwrap_or(&0) < &count {
+            return false;
+        }
+    }
+
+    true
 }
