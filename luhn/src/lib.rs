dc269a39b8e53acd34bc497b81f12c4f97f897ff
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-11 11:58:35 -0400

    luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -4,8 +4,7 @@ pub fn is_valid(code: &str) -> bool {
     let is_digits = code.chars().all(|c| c.is_ascii_digit());
     if code.len() <= 1 || !is_digits { return false; }
 
-    let reversed_code = code.chars().rev().collect::<String>();
-    reversed_code.chars().enumerate().fold(0, |sum, (i, c)| {
+    code.chars().rev().enumerate().fold(0, |sum, (i, c)| {
         let mut n = c.to_digit(10).unwrap();
         if i % 2 == 1 {
             n *= 2;
