65341473d7e405f3259a2bbfbc17f79c43194af6
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-11 11:55:49 -0400

    luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,16 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    let code = code.replace(' ', "");
+    let is_digits = code.chars().all(|c| c.is_ascii_digit());
+    if code.len() <= 1 || !is_digits { return false; }
+
+    let reversed_code = code.chars().rev().collect::<String>();
+    reversed_code.chars().enumerate().fold(0, |sum, (i, c)| {
+        let mut n = c.to_digit(10).unwrap();
+        if i % 2 == 1 {
+            n *= 2;
+            if n > 9 { n -= 9; }
+        }
+        sum + n
+    }) % 10 == 0
 }
