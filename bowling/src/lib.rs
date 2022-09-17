6792130c8ff19331dbf38b0e953227ab400a142b
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-17 03:06:22 -0400

    bowling

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -4,18 +4,71 @@ pub enum Error {
     GameComplete,
 }
 
-pub struct BowlingGame {}
+type Frame = Vec<u16>;
+
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct BowlingGame {
+    frames: Vec<Frame>,
+    current_frame: Frame,
+    score: Option<u16>,
+}
 
 impl BowlingGame {
     pub fn new() -> Self {
-       Self {}
+        Self::default()
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        if pins > 10 {
+            return Err(Error::NotEnoughPinsLeft);
+        }
+
+        if self.frames.len() == 10 {
+            return Err(Error::GameComplete);
+        }
+
+        match self.current_frame.len() {
+            0 => {
+                self.current_frame.push(pins);
+
+                let last_frame = self.frames.last_mut();
+                if let Some(frame) = last_frame {
+                    if frame.iter().sum::<u16>() == 10 {
+                        frame.push(pins);
+                    }
+                }
+            }
+            1 => {
+                let first_roll = self.current_frame[0];
+
+                if first_roll + pins > 10 {
+                    return Err(Error::NotEnoughPinsLeft);
+                }
+
+                self.current_frame.push(pins);
+                self.frames.push(self.current_frame.clone());
+                self.current_frame.clear();
+            }
+            _ => unreachable!(),
+        }
+
+
+        if self.frames.len() == 10 {
+            self.calculate_score();
+        }
+
+        Ok(())
     }
 
     pub fn score(&self) -> Option<u16> {
-        unimplemented!("Return the score if the game is complete, or None if not.");
+        self.score
+    }
+
+    fn calculate_score(&mut self) {
+        let mut score = 0;
+        for frame in self.frames.iter() {
+            score += frame.iter().sum::<u16>();
+        }
+        self.score = Some(score);
     }
 }
--- bowling/tests/bowling.rs
+++ bowling/tests/bowling.rs
@@ -7,7 +7,7 @@ fn roll_returns_a_result() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn you_cannot_roll_more_than_ten_pins_in_a_single_roll() {
     let mut game = BowlingGame::new();
 
@@ -15,7 +15,7 @@ fn you_cannot_roll_more_than_ten_pins_in_a_single_roll() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn a_game_score_is_some_if_ten_frames_have_been_rolled() {
     let mut game = BowlingGame::new();
 
@@ -28,7 +28,7 @@ fn a_game_score_is_some_if_ten_frames_have_been_rolled() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn you_cannot_score_a_game_with_no_rolls() {
     let game = BowlingGame::new();
 
@@ -36,7 +36,7 @@ fn you_cannot_score_a_game_with_no_rolls() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn a_game_score_is_none_if_fewer_than_ten_frames_have_been_rolled() {
     let mut game = BowlingGame::new();
 
@@ -49,7 +49,7 @@ fn a_game_score_is_none_if_fewer_than_ten_frames_have_been_rolled() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn a_roll_is_err_if_the_game_is_done() {
     let mut game = BowlingGame::new();
 
@@ -62,7 +62,7 @@ fn a_roll_is_err_if_the_game_is_done() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn twenty_zero_pin_rolls_scores_zero() {
     let mut game = BowlingGame::new();
 
@@ -74,7 +74,7 @@ fn twenty_zero_pin_rolls_scores_zero() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn ten_frames_without_a_strike_or_spare() {
     let mut game = BowlingGame::new();
 
@@ -87,7 +87,7 @@ fn ten_frames_without_a_strike_or_spare() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn spare_in_the_first_frame_followed_by_zeros() {
     let mut game = BowlingGame::new();
 
@@ -102,7 +102,7 @@ fn spare_in_the_first_frame_followed_by_zeros() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn points_scored_in_the_roll_after_a_spare_are_counted_twice_as_a_bonus() {
     let mut game = BowlingGame::new();
 
@@ -118,7 +118,7 @@ fn points_scored_in_the_roll_after_a_spare_are_counted_twice_as_a_bonus() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn consecutive_spares_each_get_a_one_roll_bonus() {
     let mut game = BowlingGame::new();
 
@@ -136,7 +136,7 @@ fn consecutive_spares_each_get_a_one_roll_bonus() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn if_the_last_frame_is_a_spare_you_get_one_extra_roll_that_is_scored_once() {
     let mut game = BowlingGame::new();
 
