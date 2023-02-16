0ba12a0960a2dda3d74ea83f71fd6d709623660a
soushi888 <sacha.pignot@protonmail.com>
2023-02-15 21:44:41 -0500

    acronym

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,27 +1,34 @@
 pub fn abbreviate(phrase: &str) -> String {
-	let words: Vec<&str> = phrase.split_whitespace().collect();
-	println!("{:?}", words);
+    let words: Vec<&str> = phrase.split_whitespace().collect();
 
-	let mut acronym = String::new();
-	for word in words {}
+    let mut acronym = String::new();
+    for word in words {
+        let acronym_part = get_acronym_part_from_word(word);
+        let chars: Vec<char> = acronym_part.chars().collect();
+        for char in chars {
+            acronym.push(char)
+        }
+    }
 
-	acronym
+    acronym
 }
 
-fn get_acronym_part_from_word(word: &str) -> &str {
-	let sub_words: Vec<&str> = word.split("-").collect();
-	let mut acronym_part = String::new();
-	for word in sub_words {
-		if word.to_uppercase() == word {
-			acronym_part.push_str(&word[..1]);
-		} else {
-			let mut chars = word.chars();
-			if chars.into_iter().next().unwrap().to_uppercase() {
-				acronym_part.push(chars.next().unwrap().to_uppercase())
-			}
-		}
-	}
+fn get_acronym_part_from_word(word: &str) -> String {
+    let sub_words: Vec<&str> = word.split("-").collect();
+    let mut acronym_part: Vec<String> = vec![];
+    for word in sub_words {
+        acronym_part.push(get_capitalized_first_letter(word));
+    }
 
+    acronym_part.join("")
+}
 
-	word
+fn get_capitalized_first_letter(str: &str) -> String {
+    let mut chars = str.chars();
+    let first_char = match chars.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>(),
+    };
+
+    first_char
 }
