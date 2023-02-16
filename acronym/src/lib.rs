3026be1d607865c7bcad07bdd7244fbf98a68fc8
soushi888 <sacha.pignot@protonmail.com>
2023-02-15 23:05:10 -0500

    acronym

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -18,21 +18,19 @@ pub fn abbreviate(phrase: &str) -> String {
 fn get_acronym_part_from_word(word: &str) -> String {
     let sub_words: Vec<&str> = word.split("-").collect();
     let mut acronym_part: Vec<String> = vec![];
-    println!("{:?}", sub_words);
-    for word in sub_words {
-        let capitalized_sub_words = split_by_case(word);
 
-        println!("{:?}", capitalized_sub_words);
+    for word in sub_words {
+        let capitalized_sub_words = split_by_case_without_apostrophe(word);
 
         for word in capitalized_sub_words {
-            acronym_part.push(get_capitalized_first_letter(word));
+            acronym_part.push(get_capitalized_first_letter(word.as_str()));
         }
     }
 
     acronym_part.join("")
 }
 
-fn get_capitalized_first_letter(str: String) -> String {
+fn get_capitalized_first_letter(str: &str) -> String {
     let mut chars = str.chars();
     let first_char = match chars.next() {
         None => String::new(),
@@ -42,12 +40,13 @@ fn get_capitalized_first_letter(str: String) -> String {
     first_char
 }
 
-fn split_by_case(s: &str) -> Vec<String> {
+fn split_by_case_without_apostrophe(s: &str) -> Vec<String> {
     let s = s.replace("'", "");
     let re = Regex::new(r"([A-Z][a-z]+)|([a-z]+)|([A-Z]+)").unwrap();
     let mut result = vec![];
     for cap in re.captures_iter(&s) {
         result.push(cap[0].to_string());
     }
+
     result
 }
